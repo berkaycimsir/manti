5738a03c3f024de069781a3d35c24e316027a102
berkaycimsir <berkaycimsir2003@hotmail.com>
2025-10-18 01:08:59 +0300

    initialized nextjs application

---
+++ src/server/db/index.ts
@@ -0,0 +1,16 @@
+import { drizzle, type NeonHttpClient } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import { env } from "~/env";
+import * as schema from "./schema";
+
+/**
+ * Cache the Neon client in development to avoid creating a new connection on every HMR update.
+ */
+const globalForNeon = globalThis as unknown as {
+	neonClient?: NeonHttpClient;
+};
+
+globalForNeon.neonClient ??= neon(env.DATABASE_URL);
+
+export const db = drizzle({ client: globalForNeon.neonClient, schema });
